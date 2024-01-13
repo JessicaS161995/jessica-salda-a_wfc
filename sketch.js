aebb1b08d3e319d8176ec3affa74bcef9cbde06a
JessicaS161995 <ojitos16@outlook.es>
2024-01-13 16:52:57 -0500

    creando celdas

--- sketch.js
+++ sketch.js
@@ -1,3 +1,15 @@
+const celdas = []; 
+const RETICULA = 5;
+
+let ancho; //anchura de celda
+let alto; //altura de celda
+
+const azulejos = [];
+const NA = 17; // numero de azulejos
+
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
